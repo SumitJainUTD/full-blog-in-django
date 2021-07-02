2907a6ab4c9e20df8f24fa20d011ed60cb320950
sumitjain <sumi12june@gmail.com>
2021-07-02 00:31:29 -0500

    profile model created html pages added

--- blog/user/views.py
+++ blog/user/views.py
@@ -1,7 +1,23 @@
-from django.shortcuts import render
-from django.contrib.auth.forms import UserCreationForm
+from django.shortcuts import render, redirect
+from django.contrib import messages
+from .forms import UserRegistrationForm
+from django.contrib.auth.decorators import login_required
+
+
 # Create your views here.
 
 def register(request):
-    form = UserCreationForm()
+    if request.method == 'POST':
+        form = UserRegistrationForm(request.POST)
+        if form.is_valid():
+            form.save()
+            username = form.cleaned_data.get('username')
+            messages.success(request, f'Account created and now you can login')
+            return redirect('login')
+    else:
+        form = UserRegistrationForm()
     return render(request, 'user/register.html', {'form': form})
+
+@login_required
+def profile(request):
+    return render(request, 'user/profile.html')
