9e47ee7685600b20c85b063e048804c342183e13
sumitjain <sumi12june@gmail.com>
2021-07-04 20:37:37 -0500

    bug fix base html changes create post update post delete post

--- blog/app/models.py
+++ blog/app/models.py
@@ -2,6 +2,7 @@ from django.db import models
 from django.db.models.deletion import CASCADE
 from django.utils import timezone
 from django.contrib.auth.models import User
+from django.urls import reverse
 
 # Create your models here.
 
@@ -14,3 +15,6 @@ class Post(models.Model):
 
     def __str__(self) -> str:
         return self.title
+
+    def get_absolute_url(self):
+        return reverse('post-detail', kwargs={'pk': self.pk})
--- blog/user/models.py
+++ blog/user/models.py
@@ -1,7 +1,5 @@
 from django.db import models
 from django.contrib.auth.models import User
-from django.db.models.base import Model
-from django.db.models.deletion import CASCADE
 from PIL import Image
 
 # Create your models here.
@@ -14,11 +12,10 @@ class Profile(models.Model):
         return f'{self.user.username} Profile'
 
     def save(self):
-       super.save()
+       super().save()
+       
        img = Image.open(self.image.path)
        if img.height > 300 or img.width > 300:
            output_size = (300, 300)
            img.thumbnail(output_size)
            img.save(self.image.path)
-
-    
