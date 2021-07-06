b9ee119fec3a30e272816dd61bbeb85681a38dd2
sumitjain <sumi12june@gmail.com>
2021-07-05 21:10:06 -0500

    user post page done fixed home route

--- blog/app/views.py
+++ blog/app/views.py
@@ -1,4 +1,5 @@
 from django.db import models
+from django.forms import forms
 from django.shortcuts import render
 from django.http import HttpResponse
 from django.views.generic import (
@@ -9,6 +10,9 @@ from django.views.generic import (
     DeleteView
 )
 from .models import Post
+from django.contrib.auth.mixins import LoginRequiredMixin, UserPassesTestMixin
+from django.shortcuts import render, get_object_or_404
+from django.contrib.auth.models import User
 
 posts =[
     {
@@ -37,10 +41,52 @@ class PostListView(ListView):
     template_name = 'app/home.html'
     context_object_name = 'posts'
     ordering = ['-date_posted']
+    paginate_by = 4
 
 class PostDetailView(DetailView):
     model = Post
 
+class PostCreateView(LoginRequiredMixin, CreateView):
+    model = Post
+    fields = ['title', 'content']
+
+    def form_valid(self, form):
+        form.instance.author = self.request.user
+        return super().form_valid(form)
+
+class PostUpdateView(LoginRequiredMixin, UserPassesTestMixin, UpdateView):
+    model = Post
+    fields = ['title', 'content']
+
+    def form_valid(self, form):
+        form.instance.author = self.request.user
+        return super().form_valid(form)
+
+    def test_func(self):
+        post = self.get_object()
+        if self.request.user == post.author:
+            return True
+        return False
+
+class PostDeleteView(LoginRequiredMixin, UserPassesTestMixin, DeleteView):
+    model = Post
+    success_url = '/blog'
+
+    def test_func(self):
+        post = self.get_object()
+        if self.request.user == post.author:
+            return True
+        return False
+
+class UserPostListView(ListView):
+    model = Post
+    template_name = 'app/user_posts.html'  # <app>/<model>_<viewtype>.html
+    context_object_name = 'posts'
+    paginate_by = 4
+
+    def get_queryset(self):
+        user = get_object_or_404(User, username=self.kwargs.get('username'))
+        return Post.objects.filter(author=user).order_by('-date_posted')
 
 def about(request):
     # return HttpResponse('<h1>About</h1>')
--- blog/config/urls.py
+++ blog/config/urls.py
@@ -22,7 +22,7 @@ from django.conf import settings
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('blog/', include('app.urls')),
+    path('', include('app.urls')),
     path('register/', user_view.register, name='register'),
     path('login/', auth_views.LoginView.as_view(template_name='user/login.html'), name='login'),
     path('logout/', auth_views.LogoutView.as_view(template_name='user/logout.html'), name='logout'),
