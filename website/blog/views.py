439ced414b56ea6c09a685fa96d4de4d6734d18c
SumitJainUTD <sumi12june@gmail.com>
2022-10-20 22:50:16 -0500

    blog card pagination

--- website/blog/views.py
+++ website/blog/views.py
@@ -1,5 +1,6 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
 from .models import Blog, Tag
+from django.core.paginator import PageNotAnInteger, EmptyPage, Paginator
 
 # Create your views here.
 
@@ -13,6 +14,30 @@ def home(request):
     }
     return render(request, 'home.html', context=context)
 
+
 def blogs(request):
-    print("ssss")
-    return render(request, 'blog.html')
+    queryset = Blog.objects.order_by('-created_date')
+    tags = Tag.objects.order_by('-created_date')
+    page = request.GET.get('page', 1)
+
+    paginator = Paginator(queryset, 2)
+
+    print(page)
+    try:
+        blogs = paginator.page(page)
+    except EmptyPage:
+        blogs = paginator.page(1)
+    except PageNotAnInteger:
+        blogs = paginator.page(1)
+        return redirect('blogs')
+
+    print(blogs)
+    print(paginator.per_page)
+    print(paginator.count)
+    print("blogs.number" + str(blogs.number))
+    context = {
+        "blogs": blogs,
+        "tags": tags,
+        "paginator": paginator
+    }
+    return render(request, 'blog.html', context)
